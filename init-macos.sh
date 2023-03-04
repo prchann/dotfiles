3be518ec2247e5320336cdbd5fc20ba7d5cc1dc5
igeraldchen <igeraldchen@tencent.com>
2023-03-04 06:14:07 +0000

    refactor: install.sh

---
+++ init-macos.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+
+# connect proxy server
+read -p "Proxy server is started? (Y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo;
+export http_proxy=http://127.0.0.1:1087; export https_proxy=http://127.0.0.1:1087;
+
+# check zsh
+if ! command -v zsh &> /dev/null; then
+  echo "can not found zsh, please install it at first"
+  exit 1
+fi
+
+# install omz and plugins
+echo -n "installing omz: "
+if [ -d $HOME/.oh-my-zsh ]; then
+  echo "skip"
+else
+  curl -sL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+  echo "OK"
+fi
+
+echo -n "installing zsh-autosuggestions: "
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+  echo "skip"
+else
+  git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  echo "OK"
+fi
+
+echo -n "installing zsh-syntax-highlighting: "
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+  echo "skip"
+else
+  git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  echo "OK"
+fi
+
+# install homebrew
+echo -n "installing homebrew: "
+if command -v brew &> /dev/null; then
+  echo "skip"
+else
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # source brew shell
+  [ -d /home/linuxbrew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  [ -f /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+  # homebrew index
+  brew update -q
+  echo "OK"
+fi
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+
+function brew_install_casks () {
+  for i in "$@"; do
+    read -p "install $i? (Y/n)" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || continue
+    echo -n "installing $i: "
+    brew install -q --cask "$i"
+    echo "OK"
+  done
+}
+
+function brew_install () {
+  for i in "$@"; do
+    echo -n "installing $i: "
+    brew install -q "$i"
+    echo "OK"
+  done
+}
+
+# install fonts
+echo; echo "installing fonts..."
+brew tap homebrew/cask-fonts
+casks=(
+  font-jetbrains-mono
+  font-noto-nerd-font
+)
+brew_install_casks "${casks[@]}"
+
+# install casks
+# basic tools
+echo; echo "installing system tools..."
+casks=(
+  adrive
+  rectangle
+  jietu
+  karabiner-elements
+  monitorcontrol
+  the-unarchiver
+  keycastr
+  appcleaner
+)
+brew_install_casks "${casks[@]}"
+
+# dev tools
+echo; echo "installing dev tools..."
+casks=(
+  iterm2
+  visual-studio-code
+  switchhosts
+  docker
+  insomnia
+  postman
+)
+brew_install_casks "${casks[@]}"
+
+echo; echo "installing more dev tools..."
+casks=(
+  goland
+  pycharm
+  intellij-idea
+  mysqlworkbench
+  figma
+  charles
+  royal-tsx
+)
+brew_install_casks "${casks[@]}"
+formulaes=(
+  pandoc
+)
+brew_install_casks "${formulaes[@]}"
+
+# work apps
+echo; echo "installing work apps..."
+casks=(
+  wechat
+  wechatwork
+  google-chrome
+  tencent-meeting
+  firefox-developer-edition
+  microsoft-office
+)
+brew_install_casks "${casks[@]}"
+
+# entertainment apps
+echo; echo "installing entertainment apps..."
+casks=(
+  iina
+  qqmusic
+  qqlive
+)
+brew_install_casks "${casks[@]}"
--- install.sh
+++ install.sh
@@ -1,9 +1,53 @@
 #!/usr/bin/env bash
 
-echo "installing dotfiles..."
+# config iterm2
+f=".iterm2_shell_integration.zsh"
+echo -n "$f: "
+if [ -f $HOME/$f ]; then
+    echo "skip"
+else
+    curl -sL https://iterm2.com/shell_integration/zsh -o $HOME/.iterm2_shell_integration.zsh
+    echo "OK"; echo;
+fi
 
-[ -d ~/.dotfiles ] && rm -rf ~/.dotfiles
-git clone --depth=1 git@github.com:prchann/dotfiles.git ~/.dotfiles
-cp -R ~/.dotfiles/dotfiles/. ~/
+# clone dotfiles repo
+REPO_PATH="$HOME/.dotfiles"
+FROM_BASE="$REPO_PATH/dotfiles"
+[ -d "$REPO_PATH" ] && rm -rf $REPO_PATH
+git clone -q --depth=1 git@github.com:prchann/dotfiles.git $REPO_PATH
 
-echo "dotfiles installed"
+REPLACE=false
+function copy_files () {
+    for f in "$@"; do
+        echo -n "$f: "
+        base=$(basename "$f")
+        dir=$(dirname "$f")
+        from="$FROM_BASE/$f"
+        to="$HOME/$dir"
+
+        if [ $REPLACE = false ]; then
+            [ -f "$to/$base" ] && echo "skip" && continue
+        fi
+
+        [ -d "$to" ] || mkdir -p "$to"
+        [ -f "$to/$base" ] && rm "$to/$base"
+        cp "$from" "$to"
+        echo "OK"
+    done
+}
+
+# cp dotfiles: replace if exist
+declare -a files=(
+    .config/nvim/init.vim
+    .tmux.conf
+    .vimrc
+    .zshrc
+)
+REPLACE=true; copy_files "${files[@]}"
+
+# cp dotfiles: cancel if exist
+files=(
+    .ssh/config
+    .gitconfig
+)
+REPLACE=false; copy_files "${files[@]}"
