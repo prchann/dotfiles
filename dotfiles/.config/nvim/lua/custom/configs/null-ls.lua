7e8c5f71912e5b325c93843eaf7122e1a52d56ab
igeraldchen <igeraldchen@tencent.com>
2023-08-03 01:37:36 +0000

    vi: add lsp and formating

--- dotfiles/.config/nvim/lua/custom/configs/null-ls.lua
+++ dotfiles/.config/nvim/lua/custom/configs/null-ls.lua
@@ -1,33 +1,42 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-  return
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+	-- webdev stuff
+	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format.with({
+		extra_args = {
+			"-style",
+			"{ BasedOnStyle: Google, AlignConsecutiveDeclarations: true, AlignConsecutiveAssignments: true, AlignEscapedNewlinesLeft: true, ColumnLimit: 150, IndentWidth: 2, Language: Proto }",
+		},
+	}),
 
-  -- go
-  b.formatting.gofmt,
-  b.formatting.goimports,
-  b.diagnostics.golangci_lint,
+	-- go
+	b.formatting.gofmt,
+	b.formatting.goimports,
+	b.diagnostics.golangci_lint,
 
-  -- sql
-  b.formatting.sql_formatter,
+	-- protobuf
+	-- b.formatting.buf,
+	b.diagnostics.buf,
+
+	-- sql
+	b.formatting.sql_formatter,
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
