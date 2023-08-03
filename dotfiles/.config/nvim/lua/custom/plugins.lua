523f8423e7bc129176b06a19d521f7e34173f61c
prchann <17397116+prchann@users.noreply.github.com>
2023-08-03 01:32:27 +0000

    update vi plugin

--- dotfiles/.config/nvim/lua/custom/plugins.lua
+++ dotfiles/.config/nvim/lua/custom/plugins.lua
@@ -36,9 +36,9 @@ local plugins = {
 	{
 		"nvim-tree/nvim-tree.lua",
 		opts = overrides.nvimtree,
-    keys = {
-      { "<Leader>E", "<cmd>NvimTreeFindFile<CR>", desc = "Find current file in NvimTree" },
-    }
+		keys = {
+			{ "<Leader>E", "<cmd>NvimTreeFindFile<CR>", desc = "Find current file in NvimTree" },
+		},
 	},
 
 	-- Install a plugin
@@ -50,38 +50,10 @@ local plugins = {
 		end,
 	},
 
-	-- To make a plugin not be loaded
-	-- {
-	--   "NvChad/nvim-colorizer.lua",
-	--   enabled = false
-	-- },
-
-	-- All NvChad plugins are lazy-loaded by default
-	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-	-- {
-	--   "mg979/vim-visual-multi",
-	--   lazy = false,
-	-- }
-
-  -- {
-  --   "APZelos/blamer.nvim",
-		-- event = "VeryLazy",
-  --   keys = {
-  --     { "yob", "<cmd>BlamerToggle<CR>", desc = "Toggle Git blamer"},
-  --   },
-  --   init = function()
-  --     vim.g.blamer_delay=700
-  --     vim.g.blamer_show_in_visual_modes=0
-  --     vim.g.blamer_show_in_insert_modes=0
-  --     vim.g.blamer_relative_time=1
-  --   end
-  -- },
-
-  {
-    "tpope/vim-unimpaired",
-    lazy = false,
-  },
+	{
+		"tpope/vim-unimpaired",
+		lazy = false,
+	},
 
 	{
 		"tpope/vim-surround",
@@ -93,17 +65,8 @@ local plugins = {
 		lazy = false,
 	},
 
-  -- {
-  --   "simrat39/symbols-outline.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     require("symbols-outline").setup()
-  --   end,
-  -- },
-
 	{
 		"majutsushi/tagbar",
-		-- lazy = false;
 		event = "VeryLazy",
 		keys = {
 			{ "<C-t>", "<cmd>TagbarToggle<CR>", desc = "Toggle tag bar" },
@@ -117,107 +80,43 @@ local plugins = {
 			vim.g.tagbar_show_tag_linenumbers = 1
 			vim.g.tagbar_show_data_type = 1
 			vim.g.tagbar_type_go = {
-			  ctagstype = "go",
-			  kinds = {
-			    "p:package",
-			    "i:imports:1",
-			    "c:constants",
-			    "v:variables",
-			    "t:types",
-			    "n:interfaces",
-			    "w:fields",
-			    "e:embedded",
-			    "m:methods",
-			    "r:constructor",
-			    "f:functions"
-			  },
-			  sro = ".",
-			  kind2scope = {
-			    t = "ctype",
-			    n = "ntype",
-			  },
-			  scope2kind = {
-			    ctype = "t",
-			    ntype = "n",
-			  },
-			  ctagsbin = "gotags",
-			  ctagsargs = "-sort -silent",
+				ctagstype = "go",
+				kinds = {
+					"p:package",
+					"i:imports:1",
+					"c:constants",
+					"v:variables",
+					"t:types",
+					"n:interfaces",
+					"w:fields",
+					"e:embedded",
+					"m:methods",
+					"r:constructor",
+					"f:functions",
+				},
+				sro = ".",
+				kind2scope = {
+					t = "ctype",
+					n = "ntype",
+				},
+				scope2kind = {
+					ctype = "t",
+					ntype = "n",
+				},
+				ctagsbin = "gotags",
+				ctagsargs = "-sort -silent",
 			}
 		end,
 	},
 
 	{
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = false,
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		lazy = false,
 		config = function()
 			require("todo-comments").setup()
 		end,
 	},
-
-	{
-    "yamatsum/nvim-cursorline",
-    lazy = false,
-		config = function()
-			require("nvim-cursorline").setup({
-				cursorline = {
-					enable = false,
-					timeout = 1000,
-					number = false,
-				},
-        cursorword = {
-          enable = true,
-          min_length = 3,
-          hl = {
-            underline = true
-          },
-        }
-      })
-		end,
-	},
-
-  {
-    "crispgm/nvim-go",
-    lazy = false,
-    config = function()
-      require("go").setup({
-        -- notify: use nvim-notify
-        notify = false,
-        -- auto commands
-        auto_format = true,
-        auto_lint = true,
-        -- linters: revive, errcheck, staticcheck, golangci-lint
-        linter = 'revive',
-        -- linter_flags: e.g., {revive = {'-config', '/path/to/config.yml'}}
-        linter_flags = {},
-        -- lint_prompt_style: qf (quickfix), vt (virtual text)
-        lint_prompt_style = 'qf',
-        -- formatter: goimports, gofmt, gofumpt, lsp
-        formatter = 'goimports',
-        -- maintain cursor position after formatting loaded buffer
-        maintain_cursor_pos = false,
-        -- test flags: -count=1 will disable cache
-        test_flags = {'-v'},
-        test_timeout = '30s',
-        test_env = {},
-        -- show test result with popup window
-        test_popup = true,
-        test_popup_auto_leave = false,
-        test_popup_width = 80,
-        test_popup_height = 10,
-        -- test open
-        test_open_cmd = 'edit',
-        -- struct tags
-        tags_name = 'json',
-        tags_options = {'json=omitempty'},
-        tags_transform = 'snakecase',
-        tags_flags = {'-skip-unexported'},
-        -- quick type
-        quick_type_flags = {'--just-types'},
-      })
-    end,
-  },
-
 }
 
 return plugins
