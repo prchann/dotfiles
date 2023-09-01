46a7041e871812140ece4b39f2ac142cadbe1e70
prchann <17397116+prchann@users.noreply.github.com>
2023-09-01 14:00:13 +0000

    vi: add autocmd: return to last edit position when opening files

--- dotfiles/.config/nvim/lua/custom/init.lua
+++ dotfiles/.config/nvim/lua/custom/init.lua
@@ -6,5 +6,16 @@
 --   command = "tabdo wincmd =",
 -- })
 
+-- return to last edit position when opening files
+vim.cmd([[
+    augroup restore_cursor_position
+        autocmd!
+        autocmd BufReadPost *
+            \ if line("'\"") > 1 && line("'\"") <= line("$") |
+            \   exe "normal! g`\"" |
+            \ endif
+    augroup END
+]])
+
 vim.wo.relativenumber = true
 vim.api.nvim_set_option_value("colorcolumn", "80", {})
